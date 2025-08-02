be8d9617f721c4d95f781bea3d97b0799ced90a7
linkin <pablogermanjimenez7@gmail.com>
2025-08-02 01:52:09 -0300

    added actions buttons in html through js

--- js/app.js
+++ js/app.js
@@ -6,10 +6,6 @@ const modal = new bootstrap.Modal(
   document.getElementById("addContactModalLabel") // SEGUNDO elemento seleccionado para mostrarse en window
 );
 
-btnModal.addEventListener("click", () => {
-  modal.show();
-}); // TERCERO escucha click en btnModal y muestra el contenido del modal tomado desde el dom por id por addContactModalLabel y muestra contenido div
-
 //DOM's elements
 const name = document.getElementById("name");
 const surname = document.getElementById("lastname");
@@ -26,14 +22,15 @@ const tablaContactoBody = document.getElementById("tablaContactoBody");
 
 const agenda = JSON.parse(localStorage.getItem("agenda")) || []; // si no hay nada en local storage, crea un array vacio
 
-//cargar conctato
+//add contacts to table
+//if agenda has contacts, draw them in table
 const cargarContactos = () => {
   if (agenda.length !== 0) {
     agenda.map((itemContacto, indice) =>
       dibujarFilas(itemContacto, indice + 1)
     );
   }
-}; //dibujar filitas
+}; //draw rows in table
 const dibujarFilas = (itemContacto, indice) => {
   tablaContactoBody.innerHTML += `
    <tr>
@@ -48,7 +45,7 @@ const dibujarFilas = (itemContacto, indice) => {
               class="img-thumbnail rounded-circle w-25 h-25">
             </td>
             <td><div class="btn-group" role="group" aria-label="Basic mixed styles example">
-  <button type="button" class="btn btn-danger my-1 mx-1"><i class="bi bi-trash"></i></button>
+  <button type="button" class="btn btn-danger my-1 mx-1"><i class="bi bi-trash" onClick="deleteContact('${itemContacto.id}')"></i></button>
   <button type="button" class="btn btn-warning my-1 mx-1"><i class="bi bi-pencil-square"></i></button>
   <button type="button" class="btn btn-primary my-1 mx-1"><i class="bi bi-eye "></i></button>
 </div></td>
@@ -90,6 +87,43 @@ const createContact = () => {
   dibujarFilas(contactNew, agenda.length);
 };
 
+// function delete contact
+window.deleteContact = (id) => {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+  }).then((result) => {
+    console.log(result)
+    
+    if (result.isConfirmed) {
+      //aqui agrego la logica para borrar
+      //tengo que buscar en que posicion esta el contacto
+      const indexContact = agenda.findIndex((contact)=> contact.id === id)
+      //console.log(indexContact)
+      //con splice borramos el elemento de  determinada posicion del array
+      agenda.splice(indexContact,1);
+      //actualizo el local storage
+      //tablaContactoBody.innerHTML = ""; //limpio el tbody
+      //cargarContactos(); //vuelvo a cargar los contactos    
+      tablaContactoBody.children[indexContact].remove() //limpio el tbody
+      saveLocalStorage()
+       
+      
+      Swal.fire({
+        title: "Deleted!",
+        text: "Your contact has been deleted.",
+        icon: "success",
+      });
+    }
+    
+  });
+};
+
 //event handlers
 const formContact = document.querySelector("#addContactForm");
 formContact.addEventListener("submit", (e) => {
@@ -97,3 +131,7 @@ formContact.addEventListener("submit", (e) => {
   createContact();
 });
 cargarContactos();
+
+btnModal.addEventListener("click", () => {
+  modal.show();
+}); // TERCERO escucha click en btnModal y muestra el contenido del modal tomado desde el dom por id por addContactModalLabel y muestra contenido div
