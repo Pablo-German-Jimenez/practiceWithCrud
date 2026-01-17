aad5d0af7450bff87e9a65c6f2c835fb3ab0d106
linkin <pablogermanjimenez7@gmail.com>
2026-01-17 20:40:45 -0300

    react added

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FooterBboyLinkinStyle from './src/components/shared/FooterBboyLinkinStyle';
+
+ReactDOM.createRoot(document.getElementById('footer-root')).render(
+  <React.StrictMode>
+    <FooterBboyLinkinStyle />
+  </React.StrictMode>
+);
